2cee35a39bb53dc766619c6e21d97082e1ecae01
Adrian Bateman <abateman@nomensa.com>
2023-08-07 14:27:45 +0100

    ECCI-386: Ready for tidy.

--- modules/custom/ecc_auth/js/popunder.js
+++ modules/custom/ecc_auth/js/popunder.js
@@ -4,7 +4,7 @@ const url ="https://essex-intranet.ddev.site/ecc-auth?destination=/ecc-auth/logg
 
 const features="width=500,height=550,top=300,left=250,scrollbars=1,resizable=1,toolbar=0,menubar=0,status=1,directories=0"
 
-function cookieExists(name) {
+function ccccookieExists(name) {
   const cookieArray = document.cookie.split(';');
 
   for (cookie of cookieArray) {
@@ -15,24 +15,86 @@ function cookieExists(name) {
   return false;
 }
 
-function loadpopunder(){
-  console.log('loadornot')
+
+function getCookie(name) {
+  return cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(name + "="))
+    ?.split("=")[1];
+}
+
+function cookieExists(name) {
+  return !!getCookie(name);
+
+}
+function checkCookie( cookieToBeSearched){
+  const cookie = document.cookie;
+  if(cookie === "" || cookie === undefined){
+    return false
+  }
+  let res = cookie.split(";").some(cookie => {
+    let eachCookie = cookie.split("=");
+    return eachCookie[0].trim() === cookieToBeSearched
+  });
+  return res;
+}
+
+function doAuth() {
+  console.log('doAuth')
+
+  //const data = response.json();
+  data.auth;
+  console.log(data);
+  console.log(data.auth);
+  return data.auth;
+}
+
+async function callAuth() {
+  let url = 'https://essex-intranet.ddev.site/api/internal/v1/ecc_auth';
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    // TODO: Remove.
+    console.log(error);
+  }
+}
+
+async function loadpopunder(){
+  console.log('loadpopunder');
+
 
   const currentUrl = window.location.href;
+  // Do not run on the popupunder url.
   if (currentUrl.includes('/ecc-auth')) {
+    return;
+  }
+
+  // Do not run if the user hasn't logged in before on the Essex network.
+  if (!cookieExists('essex-user')) {
+    return;
+  }
+
+
+  let doit = await callAuth();
+  // TODO: Remove.
+  console.log(doit);
+  if (!doit || !doit.auth) {
     return;
   }
 
   winPopunder=window.open(url,"Window", features)
   winPopunder.blur()
 
+  // Reload after user has been logged in.
   window.addEventListener('message', (e) => {
     const { loggedin } = e.data;
     if (loggedin) {
-      console.info('User has logged in from the other window');
+      // TODO: Remove.
+      console.log('User has logged in from the other window');
       window.location.reload();
     }
   });
 }
 
-loadpopunder()
+loadpopunder();
--- modules/custom/ecc_auth/src/Plugin/rest/resource/EccAuthAccess.php
+++ modules/custom/ecc_auth/src/Plugin/rest/resource/EccAuthAccess.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\ecc_auth\Plugin\rest\resource;
 
+use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -33,11 +34,25 @@ class EccAuthAccess extends ResourceBase {
   protected $currentUser;
 
   /**
+   * Config factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * @var \Symfony\Component\HttpFoundation\RequestStack
+   */
+  protected $requestStack;
+
+  /**
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     $instance = parent::create($container, $configuration, $plugin_id, $plugin_definition);
     $instance->currentUser = $container->get('current_user');
+    $instance->configFactory = $container->get('config.factory');
+    $instance->requestStack = $container->get('request_stack');
     return $instance;
   }
 
@@ -48,7 +63,25 @@ class EccAuthAccess extends ResourceBase {
    *   ResourceResponse.
    */
   public function get() {
-    return new ResourceResponse(['true']);
+    $config = $this->configFactory->get('ecc_auth.settings');
+    $require_header = $config->get('require_header') ?: FALSE;
+    $auth = FALSE;
+    if ($this->currentUser->isAnonymous()) {
+      if ($require_header) {
+        $header_key = $config->get('header_key');
+        $expected_header_value = $config->get('expected_header_value');
+        if ($header_key && $expected_header_value) {
+          $actual_header_value = $this->requestStack->getCurrentRequest()->headers->get($header_key);
+          \Drupal::logger('mine')->debug('header: ' . $actual_header_value);
+        }
+        $auth = !empty($actual_header_value) && $actual_header_value === $expected_header_value;
+      }
+      else {
+        $auth = TRUE;
+      }
+  }
+
+    return new ModifiedResourceResponse(['auth' => $auth]);
   }
 
 }
