936691a5dd59702a8e607e39bcbd4ed1a8f9946c
Adrian Bateman <abateman@nomensa.com>
2023-08-16 09:20:03 +0100

    ECC-386: OpenID Connect Auto-login (#205)
    * ECCI-386: Login is working.
    
    * ECCI-386: Clean up.
    
    * ECCI-386: Start on REST for access.
    
    * ECCI-386: Ready for tidy.
    
    * ECCI-386: Renaming.
    
    * ECCI-386: Check header in JS. HTTP response Headers module.
    
    * ECCI-386: Fix permissions config.
    
    * Allow cookie. Make URLs relative.
    
    * Gitlab-ci.
    
    * ECCI-386: Replace references to popunder.
    
    * ECCI-386: Settings form.
    
    * ECCI-386: Restore request header code, add task to OpenID Connect page.
    
    * ECCI-386: Working with request header
    
    * ECCI-386: Rename module machine name to oidc_auto_login
    
    * ECCI-386: Renaming.
    
    * ECCI-386: Remove debugging.
    
    * ECCI-386: Remove http_response_headers from Composer.
    
    * ECCI-386: Composer.lock.
    
    * ECCI-386: Revert other config.
    
    * ECCI-386: Add missing lines at end of files.
    
    * ECCI-386: Missing return in JS.
    
    * ECCI-386: Temporarily restore missing REST resource plugin.
    
    * ECCI-386: Temporarily restore ecc_auth module.
    
    * ECCI-386: Remove ECC Auth.
    
    * ECCI-386: Revert gitlab-ci, remove unintended permission, add return.

---
+++ modules/custom/oidc_auto_login/js/login.js
@@ -0,0 +1,68 @@
+const autoLoginUrl = "/user/auto-login?destination=/user/auto-login/logged-in";
+
+const autoLoginUrlPattern = '/user/auto-login';
+
+const allowAutoLoginEndpoint = '/api/v1/oidc_auto_login_allowed';
+
+const autoLoginFeatures = "width=500,height=500,top=0,left=0,screenX=0,screenY=0,scrollbars=1,resizable=1,toolbar=0,menubar=0,statusbar=1";
+
+/**
+ * Get a cookie.
+ * @param {string} name
+ * @returns {string}
+ */
+function getCookie(name) {
+  return cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith(name + "="))
+    ?.split("=")[1];
+}
+
+/**
+ * Check if auto-login is allowed.
+ * Response from API endpoint must contain JSON {"allow_auto_login": true}.
+ * @returns {Promise<any|boolean>}
+ */
+async function checkAllowed() {
+  try {
+    const res = await fetch(allowAutoLoginEndpoint);
+    json = await res.json();
+
+    return json && json.allow_auto_login;
+  } catch (error) {
+    return false;
+  }
+}
+
+/**
+ * Load auto-login window if it is allowed.
+ * @returns {Promise<void>}
+ */
+async function loadAutoLoginWindow() {
+  const currentUrl = window.location.href;
+  // Do not run on the auto-login urls.
+  if (currentUrl.includes(autoLoginUrlPattern)) {
+    return;
+  }
+
+  // Do not run if auto-login has failed before in this session.
+  if (getCookie('oidc-auto-login') === 'disabled') {
+    return;
+  }
+
+  if (!(await checkAllowed())) {
+    document.cookie = "oidc-auto-login=disabled"
+    return;
+  }
+
+  winAutoLoginWindow = window.open(autoLoginUrl, "Window", autoLoginFeatures)
+  if (winAutoLoginWindow) {
+    document.cookie = "oidc-auto-login=enabled"
+    winAutoLoginWindow.blur()
+  }
+  else {
+    document.cookie = "oidc-auto-login=disabled"
+  }
+}
+
+loadAutoLoginWindow();
