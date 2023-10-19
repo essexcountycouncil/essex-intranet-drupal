7721795c13667ba62828562f6f0dc145d2d61d45
Adrian Bateman <abateman@nomensa.com>
2023-10-19 08:43:23 +0100

    ECCI-421: Disable checkbox if isAncestor Restricted is TRUE.

--- modules/custom/localgov_restricted_content/src/RestrictedContent.php
+++ modules/custom/localgov_restricted_content/src/RestrictedContent.php
@@ -27,26 +27,35 @@ class RestrictedContent implements RestrictedContentInterface {
   /**
    * {@inheritdoc}
    */
-  public function isRestricted(ContentEntityInterface $entity): bool {
-    if ($this->currentUser->isAuthenticated()) {
-      return FALSE;
+  public function isAncestorRestricted(ContentEntityInterface $entity): bool {
+    foreach ($this::PARENT_FIELDS as $parent_field_option) {
+      if ($entity->hasField($parent_field_option)) {
+        $parent_field = $parent_field_option;
+        break;
+      }
     }
+    if (isset($parent_field)) {
+      if ($parent = $entity->{$parent_field}->entity) {
+        if ($parent->hasField($this::RESTRICTED_CONTENT_FIELD)) {
+          if ($parent->{$this::RESTRICTED_CONTENT_FIELD}->value) {
+            return TRUE;
+          }
+          return $this->isAncestorRestricted($parent);
+        }
+      }
+    }
+    return FALSE;
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function isRestricted(ContentEntityInterface $entity): bool {
     if ($entity->hasField($this::RESTRICTED_CONTENT_FIELD)) {
       if ($entity->{$this::RESTRICTED_CONTENT_FIELD}->value) {
         return TRUE;
       }
-      foreach ($this::PARENT_FIELDS as $parent_field_option) {
-        if ($entity->hasField($parent_field_option)) {
-          $parent_field = $parent_field_option;
-          break;
-        }
-      }
-      if (isset($parent_field)) {
-        if ($parent = $entity->{$parent_field}->entity) {
-          return $this->isRestricted($parent);
-        }
-      }
+      return $this->isAncestorRestricted($entity);
     }
     return FALSE;
   }
--- modules/custom/localgov_restricted_content/src/RestrictedContentInterface.php
+++ modules/custom/localgov_restricted_content/src/RestrictedContentInterface.php
@@ -9,7 +9,6 @@ interface RestrictedContentInterface {
   /**
    * Check if the entity is restricted.
    *
-   * Is user is authenticated then return FALSE;
    * If localgov_restricted_content is TRUE then return TRUE.
    * if localgov_restricted_content is FALSE then recursively check the entity's
    * ancestors by following the parent field.
@@ -22,4 +21,18 @@ interface RestrictedContentInterface {
    */
   public function isRestricted(ContentEntityInterface $entity): bool;
 
+  /**
+   * Check if the entity's parent or higher ancestor is restricted.
+   *
+   * Recursively check the entity's ancestors by following the parent field.
+   * If no ancestors have localgov_restricted_content set to TRUE then return
+   * FALSE.
+   *
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *
+   * @return bool
+   *   TRUE if the entity is restricted.
+   */
+  public function isAncestorRestricted(ContentEntityInterface $entity): bool;
+  
 }
