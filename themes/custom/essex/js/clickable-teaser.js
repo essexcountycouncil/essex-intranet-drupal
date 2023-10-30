2620a68108a0bd62b4e22905ffd49ee548170e9a
Kate Leigh <kleigh@nomensa.com>
2023-10-30 16:22:19 +0000

    ECCI-418: Updates styles and html for newsroom teasers and applies JS to all news cards

--- themes/custom/essex/js/clickable-teaser.js
+++ themes/custom/essex/js/clickable-teaser.js
@@ -4,11 +4,11 @@
   
     Drupal.behaviors.mybehavior = {
       attach: function (context, settings) {
-        const newsroomTeasers = document.querySelectorAll('.newsroom-teaser');
+        const newsroomTeasers = document.querySelectorAll('.newsroom-teaser, .news-card article, .featured-news__card');
 
         newsroomTeasers.forEach(function(teaser) {
             teaser.addEventListener('click', function() {
-                const linkHref = $(this).find('.newsroom-teaser__title a').attr('href');
+                const linkHref = $(this).find('h3 a').attr('href');
 
                 if (linkHref !== '') {
                     window.location = linkHref;
