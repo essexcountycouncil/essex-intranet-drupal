7a6394bd11544a1d344573b9e012136b71c61125
Adrian Bateman <abateman@nomensa.com>
2024-02-05 12:55:14 +0000

    ECCI-545: Gitlab, .drupal-ci folder

---
+++ .drupal-ci/settings.local.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @file
+ * Local settings for GitLab CI jobs.
+ */
+
+$databases['default']['default'] = [
+  'database' => 'drupal',
+  'username' => 'root',
+  'password' => 'root',
+  'prefix' => '',
+  'host' => 'db',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+
+// Display errors.
+$config['system.logging']['error_level'] = 'verbose';
+
+if (empty($settings['hash_salt'])) {
+  $settings['hash_salt'] = 'drupal-ci';
+}
