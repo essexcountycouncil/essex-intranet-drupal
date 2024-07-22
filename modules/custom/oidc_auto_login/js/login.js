bd73373c8f35e01f4356d878c112dace064d14ef
Adrian Bateman <abateman@nomensa.com>
2024-07-22 09:39:49 +0100

    feat(ECCI-555): Use Drupal behavior and settings for OIDC auto login js.

--- modules/custom/oidc_auto_login/js/login.js
+++ modules/custom/oidc_auto_login/js/login.js
@@ -1,60 +1,71 @@
-const autoLoginUrl = "/user/auto-login?destination=";
-
-const autoLoginUrlPattern = '/user/auto-login';
-
-const allowAutoLoginEndpoint = '/api/v1/oidc_auto_login_allowed';
-
 /**
- * Get a cookie.
- * @param {string} name
- * @returns {string}
+ * @file
+ * OpenID Connect Auto Login.
  */
-function getCookie(name) {
-  return cookieValue = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith(name + "="))
-    ?.split("=")[1];
-}
+(function ($, Drupal) {
 
-/**
- * Check if auto-login is allowed.
- * Response from API endpoint must contain JSON {"allow_auto_login": true}.
- * @returns {Promise<any|boolean>}
- */
-async function checkAllowed() {
-  try {
-    const res = await fetch(allowAutoLoginEndpoint);
-    json = await res.json();
+  'use strict';
 
-    return json && json.allow_auto_login;
-  } catch (error) {
-    return false;
-  }
-}
+  const autoLoginUrl = "/user/auto-login?destination=";
 
-/**
- * Attempts auto-login if it is allowed.
- * @returns {Promise<void>}
- */
-async function autoLogin() {
-  const currentUrl = window.location.href;
-  // Do not run on the auto-login url.
-  if (currentUrl.includes(autoLoginUrlPattern)) {
-    return;
+  const autoLoginUrlPattern = '/user/auto-login';
+
+  const allowAutoLoginEndpoint = '/api/v1/oidc_auto_login_allowed';
+
+  /**
+   * Get a cookie.
+   * @param {string} name
+   * @returns {string}
+   */
+  function getCookie(name) {
+    return document.cookie
+      .split("; ")
+      .find((row) => row.startsWith(name + "="))
+      ?.split("=")[1];
   }
 
-  // Do not attempt auto-login more than once in this session.
-  if (getCookie('oidc-auto-login') === 'attempted') {
-    return;
+  /**
+   * Check if auto-login is allowed.
+   * Response from API endpoint must contain JSON {"allow_auto_login": true}.
+   * @returns {Promise<any|boolean>}
+   */
+  async function checkAllowed() {
+    try {
+      const res = await fetch(allowAutoLoginEndpoint);
+      const json = await res.json();
+
+      return json && json.allow_auto_login;
+    } catch (error) {
+      return false;
+    }
   }
-  document.cookie = "oidc-auto-login=attempted"
 
-  if (!(await checkAllowed())) {
-    return;
+  /**
+   * AutoLogin behaviour.
+   *
+   * @type {Drupal~behavior}
+   */
+  Drupal.behaviors.autoLogin = {
+    attach: function attach(context, settings) {
+      const currentUrl = window.location.href;
+      // Do not run on the auto-login url.
+      if (currentUrl.includes(autoLoginUrlPattern)) {
+        return;
+      }
+
+      // Do not attempt auto-login more than once in this session.
+      if (getCookie('oidc-auto-login') === 'attempted') {
+        return;
+      }
+      const cookieMaxAge= drupalSettings.oidc_auto_login.cookieMaxAge.toString() ?? "300";
+      document.cookie = "oidc-auto-login=attempted; max-age=" + cookieMaxAge;
+
+      if (!(checkAllowed())) {
+        return;
+      }
+
+      const redirectUrl = autoLoginUrl + window.location.pathname;
+      window.location.replace(redirectUrl);
+    }
   }
-
-  const redirectUrl = autoLoginUrl + window.location.pathname;
-  window.location.replace(redirectUrl);
-}
-
-autoLogin();
+})(jQuery, Drupal);
